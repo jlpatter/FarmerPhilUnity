da39c5b2ff9f8383543a33644f70039f226f11b5
Joshua Patterson <jleegippies@gmail.com>
2021-05-17 16:55:08 -0500

    Added player movement

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    private const float Speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxisRaw("Horizontal") > 0) {
+            _spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0) {
+            _spriteRenderer.flipX = false;
+        }
+        _rigidbody2D.MovePosition(_rigidbody2D.position + new Vector2(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime,
+                                  Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime));
+    }
+}
