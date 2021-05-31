3807fde591dbcd3740bc5a6271fa8cdaf3968db2
Joshua Patterson <jleegippies@gmail.com>
2021-05-31 12:04:29 -0500

    WIP working on grub movement

--- Assets/Scripts/GrubBehavior.cs
+++ Assets/Scripts/GrubBehavior.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
 public class GrubBehavior : MonoBehaviour {
+    private Animator _animator;
+    private const float Speed = 2.0f;
+    
     // Start is called before the first frame update
     private void Start() {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update() {
-        
+        _animator.SetInteger("MoveState", 0);
+        transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);
+        _animator.SetFloat("Speed", Speed * Time.deltaTime);
     }
 }
