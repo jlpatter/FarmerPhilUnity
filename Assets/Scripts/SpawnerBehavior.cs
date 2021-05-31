02516ada33219d21ffead2ef15e04a1718f9eec3
Joshua Patterson <jleegippies@gmail.com>
2021-05-31 10:30:12 -0500

    Filled the field with wheat and fixed some misc problems

--- Assets/Scripts/SpawnerBehavior.cs
+++ Assets/Scripts/SpawnerBehavior.cs
@@ -6,22 +6,22 @@ public class SpawnerBehavior : MonoBehaviour {
     public Tilemap tilemap;
 
     // Start is called before the first frame update
-    void Start() {
+    private void Start() {
+        SpawnWheat();
+    }
+
+    private void SpawnWheat() {
         // Find wheat's x start location
-        var tileNameX = "dirt";
         var wheatXStartLocation = 0;
-        while (tileNameX.Equals("dirt")) {
+        while (tilemap.GetSprite(new Vector3Int(wheatXStartLocation, 0, 0)).name.Equals("dirt")) {
             wheatXStartLocation--;
-            tileNameX = tilemap.GetSprite(new Vector3Int(wheatXStartLocation, 0, 0)).name;
         }
         wheatXStartLocation++;
 
         // Find wheat's y start location
-        var tileNameY = "dirt";
         var wheatYLocation = 0;
-        while (tileNameY.Equals("dirt")) {
+        while (tilemap.GetSprite(new Vector3Int(0, wheatYLocation, 0)).name.Equals("dirt")) {
             wheatYLocation--;
-            tileNameY = tilemap.GetSprite(new Vector3Int(0, wheatYLocation, 0)).name;
         }
         wheatYLocation++;
 
@@ -31,6 +31,7 @@ public class SpawnerBehavior : MonoBehaviour {
         while (tilemap.GetSprite(new Vector3Int(0, wheatYLocation, 0)).name.Equals("dirt")) {
             while (tilemap.GetSprite(new Vector3Int(wheatXLocation, 0, 0)).name.Equals("dirt")) {
                 Instantiate(wheatPrefab, new Vector3(wheatXLocation + wheatSize.x / 2.0f, wheatYLocation + wheatSize.y / 2.0f, 0.0f), Quaternion.identity);
+                Instantiate(wheatPrefab, new Vector3(wheatXLocation + 0.5f + wheatSize.x / 2.0f, wheatYLocation + wheatSize.y / 2.0f, 0.0f), Quaternion.identity);
                 wheatXLocation++;
             }
             wheatXLocation = wheatXStartLocation;
