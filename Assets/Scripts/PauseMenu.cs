7f028a7ef273c89627601b92d1ad2db0ff6531de
Joshua Patterson <jleegippies@gmail.com>
2021-06-01 16:26:43 -0500

    Added a pause menu that shows up when hitting escape

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerBehavior : MonoBehaviour {
+
+    [FormerlySerializedAs("PauseCanvas")]
+    public GameObject pauseCanvas;
+    
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
+    
     private const float Speed = 20.0f;
 
     // Start is called before the first frame update
@@ -13,6 +19,17 @@ public class PlayerBehavior : MonoBehaviour {
 
     // Update is called once per frame
     private void Update() {
+        ShowPauseMenu();
+        MovePlayer();
+    }
+
+    private void ShowPauseMenu() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            pauseCanvas.SetActive(!pauseCanvas.activeSelf);
+        }
+    }
+
+    private void MovePlayer() {
         if (Input.GetAxisRaw("Horizontal") > 0) {
             _spriteRenderer.flipX = true;
         }
@@ -21,6 +38,6 @@ public class PlayerBehavior : MonoBehaviour {
             _spriteRenderer.flipX = false;
         }
         _rigidbody2D.MovePosition(_rigidbody2D.position + new Vector2(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime,
-                                  Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime));
+            Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime));
     }
 }
