4501b9c22f117c8ae67043b7779662da898b0055
Joshua Patterson <jleegippies@gmail.com>
2021-06-03 20:27:53 -0500

    WIP Added ability to purchase spray cans, need to fix placement on spawn

--- Assets/Scripts/Menus/ShopMenu.cs
+++ Assets/Scripts/Menus/ShopMenu.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 namespace Menus {
     public class ShopMenu : MonoBehaviour {
         public bool isShop;
+        public GameObject player;
+        public GameObject sprayCanPrefab;
         public SpawnerBehavior spawnerBehavior;
         public GrubArmyBehavior grubArmyBehavior;
+
+        private PlayerBehavior _playerBehavior;
+
+        private void Start() {
+            _playerBehavior = player.GetComponent<PlayerBehavior>();
+        }
 
         public void ContinueGame() {
             SetIsShop(false);
             grubArmyBehavior.numOfGrubs = 5;
             spawnerBehavior.SpawnGrubs();
+        }
+
+        public void PurchaseSprayCan() {
+            var newSprayCan = Instantiate(sprayCanPrefab, new Vector3(player.transform.position.x, player.transform.position.y), Quaternion.identity, player.transform);
+            newSprayCan.SetActive(false);
+            _playerBehavior.AddWeapon(newSprayCan);
         }
 
         public void SetIsShop(bool isShopValue) {
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Menus;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -7,7 +9,6 @@ public class PlayerBehavior : MonoBehaviour {
     [FormerlySerializedAs("PauseCanvas")]
     public GameObject pauseCanvas;
     public GameObject bat;
-    public GameObject sprayCan;
     public PlayerHealthBar healthBar;
     public PauseMenu pauseMenu;
     public StartMenu startMenu;
@@ -17,9 +18,8 @@ public class PlayerBehavior : MonoBehaviour {
     private int _currentWeaponIndex;
     private bool _isTouchingGrubby;
     private bool _hasGoneRight;
+    private List<Tuple<GameObject, SpriteRenderer>> _weapons;
     private SpriteRenderer _spriteRenderer;
-    private SpriteRenderer _batSpriteRenderer;
-    private SpriteRenderer _sprayCanSpriteRenderer;
 
     private const float MaxHealth = 100.0f;
     private const float GrubStrength = 0.05f;
@@ -32,8 +32,8 @@ public class PlayerBehavior : MonoBehaviour {
         _isTouchingGrubby = false;
         _hasGoneRight = false;
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _batSpriteRenderer = bat.GetComponent<SpriteRenderer>();
-        _sprayCanSpriteRenderer = sprayCan.GetComponent<SpriteRenderer>();
+        _weapons = new List<Tuple<GameObject, SpriteRenderer>>();
+        AddWeapon(bat);
     }
 
     // Update is called once per frame
@@ -42,12 +42,20 @@ public class PlayerBehavior : MonoBehaviour {
         if (!pauseMenu.isPaused && !startMenu.isStart && !shopMenu.isShop) {
             TakeDamage();
             SwitchWeapon();
-            if (gameObject.transform.GetChild(_currentWeaponIndex).name.Equals("Bat")) {
-                sprayCan.SetActive(false);
+            if (_weapons[_currentWeaponIndex].Item1.name.Contains("Bat")) {
+                foreach (var (weapon, _) in _weapons) {
+                    if (!weapon.name.Contains("Bat")) {
+                        weapon.SetActive(false);
+                    }
+                }
                 SwingBat();
             }
-            else if (gameObject.transform.GetChild(_currentWeaponIndex).name.Equals("SprayCan")) {
-                bat.SetActive(false);
+            else if (_weapons[_currentWeaponIndex].Item1.name.Contains("SprayCan")) {
+                foreach (var (weapon, _) in _weapons) {
+                    if (!weapon.name.Contains("SprayCan")) {
+                        weapon.SetActive(false);
+                    }
+                }
                 SprayCan();
             }
             MovePlayer();
@@ -64,6 +72,10 @@ public class PlayerBehavior : MonoBehaviour {
         if (other.gameObject.name.Contains("Grubby")) {
             _isTouchingGrubby = false;
         }
+    }
+
+    public void AddWeapon(GameObject w) {
+        _weapons.Add(new Tuple<GameObject, SpriteRenderer>(w, w.GetComponent<SpriteRenderer>()));
     }
 
     private void ShowPauseMenu() {
@@ -83,52 +95,56 @@ public class PlayerBehavior : MonoBehaviour {
     private void SwitchWeapon() {
         if (Input.GetKeyDown(KeyCode.E)) {
             _currentWeaponIndex++;
-            if (_currentWeaponIndex >= gameObject.transform.childCount) {
+            if (_currentWeaponIndex >= _weapons.Count) {
                 _currentWeaponIndex = 0;
             }
         }
         else if (Input.GetKeyDown(KeyCode.Q)) {
             _currentWeaponIndex--;
             if (_currentWeaponIndex < 0) {
-                _currentWeaponIndex = gameObject.transform.childCount - 1;
+                _currentWeaponIndex = _weapons.Count - 1;
             }
         }
     }
 
     private void SwingBat() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            bat.SetActive(true);
+            _weapons[_currentWeaponIndex].Item1.SetActive(true);
         }
     }
 
     private void SprayCan() {
         if (Input.GetKey(KeyCode.Space)) {
-            sprayCan.SetActive(true);
+            _weapons[_currentWeaponIndex].Item1.SetActive(true);
         }
         else {
-            sprayCan.SetActive(false);
+            _weapons[_currentWeaponIndex].Item1.SetActive(false);
         }
     }
 
     private void MovePlayer() {
         if (Input.GetAxisRaw("Horizontal") > 0) {
             _spriteRenderer.flipX = true;
-            _batSpriteRenderer.flipX = true;
-            _sprayCanSpriteRenderer.flipX = true;
+            foreach (var weapon in _weapons) {
+                weapon.Item2.flipX = true;
+            }
             if (!_hasGoneRight) {
-                bat.transform.localPosition = new Vector3(-bat.transform.localPosition.x, bat.transform.localPosition.y, bat.transform.localPosition.z);
-                sprayCan.transform.localPosition = new Vector3(-sprayCan.transform.localPosition.x, sprayCan.transform.localPosition.y, sprayCan.transform.localPosition.z);
+                foreach (var (weapon, _) in _weapons) {
+                    weapon.transform.localPosition = new Vector3(-weapon.transform.localPosition.x, weapon.transform.localPosition.y, weapon.transform.localPosition.z);
+                }
                 _hasGoneRight = true;
             }
         }
 
         if (Input.GetAxisRaw("Horizontal") < 0) {
             _spriteRenderer.flipX = false;
-            _batSpriteRenderer.flipX = false;
-            _sprayCanSpriteRenderer.flipX = false;
+            foreach (var weapon in _weapons) {
+                weapon.Item2.flipX = false;
+            }
             if (_hasGoneRight) {
-                bat.transform.localPosition = new Vector3(-bat.transform.localPosition.x, bat.transform.localPosition.y, bat.transform.localPosition.z);
-                sprayCan.transform.localPosition = new Vector3(-sprayCan.transform.localPosition.x, sprayCan.transform.localPosition.y, sprayCan.transform.localPosition.z);
+                foreach (var (weapon, _) in _weapons) {
+                    weapon.transform.localPosition = new Vector3(-weapon.transform.localPosition.x, weapon.transform.localPosition.y, weapon.transform.localPosition.z);
+                }
                 _hasGoneRight = false;
             }
         }
