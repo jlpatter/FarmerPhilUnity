7e8b296532900a321332da6a616e0fb45229c8ba
Joshua Patterson <jleegippies@gmail.com>
2021-06-05 11:20:59 -0500

    Added restart button

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Menus {
     public class PauseMenu : MonoBehaviour {
         public bool IsPaused { get; set; }
 
+        public void RestartGame() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
         public void QuitGame() {
             Application.Quit();
         }
